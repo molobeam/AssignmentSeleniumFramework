54641de06ff3306205e87ec49305efa480588731
molobeam <molobeam@gmail.com>
2025-10-02 15:42:32 +0200

    created shippingexpress method

--- src/test/java/PricingPanel/Step2ExtrasPricing.java
+++ src/test/java/PricingPanel/Step2ExtrasPricing.java
@@ -8,11 +8,14 @@ public class Step2ExtrasPricing {
     WebDriver driver;
 @FindBy(id="shipping-express")
 WebElement shippingMethodExpress_id;
+@FindBy(id="warranty-option-1yr")
+WebElement warrantyOption1yr_id;
     public Step2ExtrasPricing(WebDriver driver) {
         this.driver = driver;
     }
     public void shippingMethodExpress(){
     shippingMethodExpress_id.click();
+    warrantyOption1yr_id.click();
     }
 
 
